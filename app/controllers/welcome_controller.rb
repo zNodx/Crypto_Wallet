60742d5e207fde9546bf13b317cfd23cacc820b7
Mateus Santos <ffrmateus@gmail.com>
2022-03-02 19:12:19 -0300

    Utilizando a biblioteca JS(vendor)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @meu_nome=params[:nome]
+    cookies[:curso] = "Curso de Ruby on Rails [COOKIE]"
+    session[:curso] = "Curso de Ruby on Rails [SESSION]"
+    @meu_nome = params[:nome]
+    @curso = params[:curso]
   end
 end
